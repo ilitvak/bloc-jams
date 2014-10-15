0796b77566ec212c024c6479e1e690962b039cb6
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-15 19:20:41 +0000

    Added an overlay to collection view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -187,7 +187,9 @@ require('./album');
  var buildAlbumThumbnail = function() {
     var template =
         '<div class="collection-album-container col-md-2">'
-      + '  <img src="/images/album-placeholder.png"/>'
+        + '  <div class="collection-album-image-container">'
+   + '    <img src="/images/album-placeholder.png"/>'
+   + '  </div>'
       + '  <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -203,14 +205,40 @@ require('./album');
    return $(template);
  };
 
- var updateCollectionView = function() {
+ var buildAlbumOverlay = function(albumURL) {
+    var template =
+        '<div class="collection-album-image-overlay">'
+      + '  <div class="collection-overlay-content">'
+      + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+      + '      <i class="fa fa-play"></i>'
+      + '    </a>'
+      + '    &nbsp;'
+      + '    <a class="collection-overlay-button">'
+      + '      <i class="fa fa-plus"></i>'
+      + '    </a>'
+      + '  </div>'
+      + '</div>'
+      ;
+    return $(template);
+  };
+
+var updateCollectionView = function() {
    var $collection = $(".collection-container .row");
    $collection.empty();
  
-   for (var i = 0; i < 33; i++) {
+for (var i = 0; i < 33; i++) {
      var $newThumbnail = buildAlbumThumbnail();
-     $collection.append($newThumbnail);
+    $collection.append($newThumbnail);
    }
+var onHover = function(event) {
+  $(this).append(buildAlbumOverlay("/album.html"));
+  };
+  
+var offHover = function(event) {
+  $(this).find('.collection-album-image-overlay').remove();
+  };
+  
+$collection.find('.collection-album-image-container').hover(onHover, offHover);
  };
  
  
@@ -221,6 +249,7 @@ if (document.URL.match(/\/collection.html/)) {
      updateCollectionView();
    });
 }
+   
 });
 
 ;require.register("scripts/landing", function(exports, require, module) {
