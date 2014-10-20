e72a46f2d5d84652491e8a001360b60a0f9c0b8a
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-20 20:14:21 +0000

    Converted collection view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,9 +2,25 @@
 //require('./album');
 //require('./collection');
 //require('./profile');
+ // Example Album
+ var albumPicasso = {
+   name: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: '/images/album-placeholder.png',
  
-blocJams = angular.module('BlocJams', ['ui.router']);
- blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   songs: [
+       { name: 'Blue', length: '4:26' },
+       { name: 'Green', length: '3:14' },
+       { name: 'Red', length: '5:01' },
+       { name: 'Pink', length: '3:21'},
+       { name: 'Magenta', length: '2:15'}
+     ]
+ };
+
+ blocJams = angular.module('BlocJams', ['ui.router']);
+  blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
  
    $stateProvider.state('landing', {
@@ -12,26 +28,45 @@ blocJams = angular.module('BlocJams', ['ui.router']);
      controller: 'Landing.controller',
      templateUrl: '/templates/landing.html'
    });
- }]);
- 
+
+
+   $stateProvider.state('collection', {
+    url: '/collection',
+    controller: 'Collection.controller',
+    templateUrl: '/templates/collection.html'
+   }); 
+    
+ }]); 
+
 // This is a cleaner way to call the controller than crowding it on the module definition.
  blocJams.controller('Landing.controller', ['$scope', function($scope) {
-  $scope.subText = "Turn the Music Up!";
+  $scope.subText = "Turn the music Up!";
   
-   $scope.subTextClicked = function() {
-     $scope.subText += '!';
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
    };
-  
-     $scope.albumURLs = [
-     '/images/album-placeholders/album-1.jpg',
-     '/images/album-placeholders/album-2.jpg',
-     '/images/album-placeholders/album-3.jpg',
-     '/images/album-placeholders/album-4.jpg',
-     '/images/album-placeholders/album-5.jpg',
-     '/images/album-placeholders/album-6.jpg',
-     '/images/album-placeholders/album-7.jpg',
-     '/images/album-placeholders/album-8.jpg',
-     '/images/album-placeholders/album-9.jpg',
+   
+   $scope.albumURLs = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg'
    ];
- }]);
+}]);   
+  blocJams.controller('Collection.controller', ['$scope', function($scope) {
+    $scope.albums = [];
+   for (var i = 0; i < 33; i++) {
+    $scope.albums.push(angular.copy(albumPicasso));
+   }
+ 
+}]);
+
+ 
+     
+
 
