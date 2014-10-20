58a43f35e7d71be34dc59171fa9377c15af37744
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-20 13:45:14 +0000

    Converted landing page to Angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -271,10 +271,32 @@ $(document).ready(function() {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require('./collection');
-require('./album');
-require("./profile");
+//require('./landing');
+//require('./album');
+//require('./collection');
+//require('./profile');
+ 
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.subText = "Turn the Music Up!";
+  
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
+  
+     $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+ }]);
+
+
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
