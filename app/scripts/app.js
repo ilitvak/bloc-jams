7ca4d6499643c2b2cf2bc80d86f8e26d31ae7f12
Irwin Litvak <irwin.litvak@gmail.com>
2014-10-21 17:23:59 +0000

    Updated slider to update music location!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,32 +11,32 @@
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
 
- blocJams = angular.module('BlocJams', ['ui.router']);
+  blocJams = angular.module('BlocJams', ['ui.router']);
   blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-   $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
  
-   $stateProvider.state('landing', {
-     url: '/',
-     controller: 'Landing.controller',
-     templateUrl: '/templates/landing.html'
+    $stateProvider.state('landing', {
+      url: '/',
+      controller: 'Landing.controller',
+      templateUrl: '/templates/landing.html'
    });
 
 
-   $stateProvider.state('collection', {
-    url: '/collection',
-    controller: 'Collection.controller',
-    templateUrl: '/templates/collection.html'
+    $stateProvider.state('collection', {
+     url: '/collection',
+     controller: 'Collection.controller',
+     templateUrl: '/templates/collection.html'
    }); 
     
-   $stateProvider.state('album', {
+    $stateProvider.state('album', {
      url: '/album',
      templateUrl: '/templates/album.html',
      controller: 'Album.controller'
@@ -158,7 +158,13 @@ return {
     this.setSong(this.currentAlbum, song);
      },
      
-     
+    seek: function(time) {
+      // Checks to make sure that a sound file is playing before seeking.
+     if(currentSoundFile) {
+      // Uses a Buzz method to set the time of the song.
+     currentSoundFile.setTime(time);
+      }
+     },
   setSong: function(album, song) {
     if (currentSoundFile) {
         currentSoundFile.stop();
@@ -188,22 +194,48 @@ return {
     return offsetXPercent;
     }
     
-   
+     var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+  
     return {
       templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
       replace: true,
       restrict: 'E',
-        scope: {}, // Creates a scope that exists only in this directive.
+      scope: {
+      onChange: '&'
+      },
         link: function(scope, element, attributes) {
         // These values represent the progress into the song/volume bar, and its max value.
         // For now, we're supplying arbitrary initial and max values.
         scope.value = 0;
-        scope.max = 200;  
+        scope.max = 100;
         var $seekBar = $(element);
-        
+  
+        attributes.$observe('value', function(newValue) {
+          scope.value = numberFromValue(newValue, 0);
+        });
+
+        attributes.$observe('max', function(newValue) {
+          scope.max = numberFromValue(newValue, 100) || 100;
+        });
+  
+   
         var percentString = function () {
-          percent = Number(scope.value) / Number(scope.max)  * 100;
-          return percent + "%";
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+          percent = value / max * 100;
+         return percent + "%";
         }
  
         scope.fillStyle = function() {
@@ -216,12 +248,14 @@ return {
         scope.onClickSlider = function(event) {
           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
           scope.value = percent * scope.max;
+          notifyCallback(scope.value);
        }
         scope.trackThumb = function() {
           $document.bind('mousemove.thumb', function(event){
           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
           scope.$apply(function(){
           scope.value = percent * scope.max;
+          notifyCallback(scope.value);
           });
         });
  
@@ -232,7 +266,14 @@ return {
           });
         };
 
-        
+       // Place this as the last function defined in the 'link' function of the directive.
+       var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
+       };
+          
+
    
     }
    };
